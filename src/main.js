7036c988e3a6de432a6d2d6a5ab4b67d6d801a2f
Sadick Sulley <sulleysadick766@gmail.com>
2025-07-16 13:53:07 +0000

    added more js functionality and changed some styles in the index.html

--- src/main.js
+++ src/main.js
@@ -5,30 +5,43 @@ import "./style.css";
 ("use strict");
 
 // Selecting Elements
-const bodyEl = document.body;
 const shareButton = document.querySelector(".share-button");
 const infoContainer = document.querySelector(".info-div");
-const personInfo = document.querySelector(".person-info");
 const shareIcons = document.querySelector(".share-icons");
-const screenWidth = window.innerWidth;
-
-const mobileShareButton = document.querySelector(".mobile-share-button");
+const closeShareButton = document.querySelector(".mobile-share-button");
 
 // hiding the shareIcons when the page loads
 shareIcons.classList.add("hidden");
 
-shareButton.addEventListener("click", () => {
-  if (screenWidth >= "768") {
-    bodyEl.style.backgroundColor = "hsl(174, 77%, 80%)";
-    shareIcons.classList.toggle("hidden");
-  } else {
-    shareIcons.classList.toggle("hidden");
-    infoContainer.classList.add("hidden");
-    bodyEl.style.backgroundColor = "hsl(224, 65%, 95%)";
-  }
-});
+// Function: To update User interface based on screeen size of the window
+const isMobiie = () => {
+  return window.innerWidth <= 768;
+};
 
-mobileShareButton.addEventListener("click", () => {
-  shareIcons.classList.add("hidden");
+//Function: To show or hide the shareIcons and infoContainer based on the screen size or when the page loads
+const updateUserInterface = () => {
+  // mobile view
+  if (isMobiie()) {
+    infoContainer.classList.toggle("hidden");
+    shareIcons.classList.remove("hidden");
+  } else {
+    // desktop view
+    infoContainer.classList.remove("hidden");
+    shareIcons.classList.toggle("hidden");
+  }
+};
+
+// calling the updateUserInterface function so it works when the page loads
+updateUserInterface();
+
+// add EvenListener to the shareButton
+shareButton.addEventListener("click", updateUserInterface);
+
+//upddte user interface based on the screen size
+window.addEventListener("resize", updateUserInterface);
+
+// close share icons
+closeShareButton.addEventListener("click", () => {
   infoContainer.classList.remove("hidden");
+  shareIcons.classList.add("hidden");
 });
